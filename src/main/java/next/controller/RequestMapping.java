1025e2a951487eeb0dd174ca4e6131b2474d9ce0
khg339 <khg33333339@gmail.com>
2023-01-26 15:18:49 +0900

    refactor : 요청 URL과 컨트롤러 매핑

---
+++ src/main/java/next/controller/RequestMapping.java
@@ -0,0 +1,35 @@
+package next.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestMapping {
+    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+    private Map<String, Controller> mappings = new HashMap<>();
+
+    public void initMapping(){
+        mappings.put("/", new HomeController());
+        mappings.put("/users/form", new ForwardController("/user/form.jsp"));
+        mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
+        mappings.put("/users", new ListUserController());
+        mappings.put("/users/login", new LoginController());
+        mappings.put("/users/create", new CreateUserController());
+        mappings.put("/users/logout", new LogoutController());
+        mappings.put("/users/profile", new ProfileController());
+        mappings.put("/users/updateForm", new UpdateFormUserController());
+        mappings.put("/users/update", new UpdateUserController());
+
+        log.info("Initialized Request Mapping!");
+    }
+
+    public Controller findController(String url){
+        return mappings.get(url);
+    }
+
+    public void put(String url, Controller controller){
+        mappings.put(url, controller);
+    }
+}
