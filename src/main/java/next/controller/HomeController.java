8b7523220f49c2c1951b045c2b2d0982fd11f419
khg339 <khg33333339@gmail.com>
2023-01-27 15:47:42 +0900

    refactor : Controller 에 UserDao 적용

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import core.db.DataBase;
 import core.mvc.Controller;
+import next.dao.UserDao;
+import next.model.User;
 
 public class HomeController implements Controller {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        req.setAttribute("users", DataBase.findAll());
+        UserDao userDao = new UserDao();
+        req.setAttribute("users", userDao.findAll());
         return "home.jsp";
     }
 }
