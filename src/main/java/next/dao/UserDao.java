5d942b33e0d311086e493500a46c7f31b8a38390
khg339 <khg33333339@gmail.com>
2023-01-27 15:43:10 +0900

    feat : userDao update() 추가

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -23,7 +23,7 @@ public class UserDao {
             pstmt.setString(3, user.getName()); //세번째 물음표 값
             pstmt.setString(4, user.getEmail()); //네번째 물음표 값
 
-            pstmt.executeUpdate(); //값을 모두 대입한 결과 실행
+            pstmt.executeUpdate(); //값을 모두 대입한 결과 실행(insert 문이기 때문에 executeUpdate)
         } finally {
             if (pstmt != null) {
                 pstmt.close();
@@ -37,6 +37,28 @@ public class UserDao {
 
     public void update(User user) throws SQLException { //update 메소드
         // TODO 구현 필요함.
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = ConnectionManager.getConnection();
+            String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+            pstmt.executeUpdate();//값을 모두 대입한 결과 실행(update 문이기 때문에 executeUpdate)
+        }finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException { //모든 user 반환 메소드
@@ -50,7 +72,7 @@ public class UserDao {
             con = ConnectionManager.getConnection();
             String sql = "SELECT * FROM USERS";
             pstmt = con.prepareStatement(sql);
-            rs = pstmt.executeQuery();
+            rs = pstmt.executeQuery(); //값을 모두 대입한 결과 실행(select 문이기 때문에 executeQuery)
 
             while(rs.next()){
                 User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
@@ -81,7 +103,7 @@ public class UserDao {
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
 
-            rs = pstmt.executeQuery(); //결과 집합 저장
+            rs = pstmt.executeQuery(); ////값을 모두 대입한 결과 실행(select 문이기 때문에 executeQuery)
 
             User user = null;
             if (rs.next()) { //결과 집합을 하나씩 검사(다음 값이 있으면 true, 없으면 false 반환)
